01e2a3bcccfd6dca759ac19246a3943d3140fcf2
Bass, David <d-bass@outlook.com>
2021-11-02 15:20:52 -0400

    Update Translator

---
+++ eTicketz/Models/Cinema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketz.Models
+{
+    public class Cinema
+    {
+        // Unique Key for Cinema Class
+
+        [Key]
+        public int Id { get; set; }
+        public string  Logo { get; set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ eTicketz/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using eTicketz.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketz.Models
+{
+    public class Movie
+    {
+     
+        [Key] // Data Anotation 
+     // Primary Key ID for Movie Class Table
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Description { get; set; }
+
+        public Double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string CinamaName { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
